c6e43cad30ccd58ac512b7e900985dd9849c62f8
Tushar Sutar <tushars1027@gmail.com>
2025-04-09 16:18:48 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name = 'Medical AI Chatbot',
+    version= '0.0.0',
+    author= 'Tushar Sutar',
+    author_email= 'tushars1027@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ template.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+    "src/__init__.pyi"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"creating directing: {filedir} for the files: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filepath} is already exits!")
